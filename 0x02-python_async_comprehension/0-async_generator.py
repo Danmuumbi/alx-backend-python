5ec374492d59bc2f43333b4877a60ffda06578e5
Danmuumbi <muuomuumbi@gmail.com>
2024-07-09 12:46:13 +0300

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,3 +1,18 @@
+# #!/usr/bin/env python3
+# '''Task 0's module.
+# '''
+# import asyncio
+# import random
+# from typing import AsyncGenerator
+
+
+# async def async_generator() -> AsyncGenerator[float, None]:
+#     '''Generates a sequence of 10 numbers.
+#     '''
+#     for _ in range(10):
+#         await asyncio.sleep(1)
+#         yield random.random() * 10
+
 #!/usr/bin/env python3
 '''Task 0's module.
 '''
@@ -11,4 +26,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
